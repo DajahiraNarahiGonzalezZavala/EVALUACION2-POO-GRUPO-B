96f43703c73af8a4970dcf2a8f358d2c3c922177
DajahiraNarahiGonzalezZavala <101366240+DajahiraNarahiGonzalezZavala@users.noreply.github.com>
2022-04-28 09:56:43 -0600

    Add files via upload

---
+++ NetBeansProjects/EVA2_19_CLASES_ANONIMAS/src/eva2_19_clases_anonimas/EVA2_19_CLASES_ANONIMAS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_19_clases_anonimas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_19_CLASES_ANONIMAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
